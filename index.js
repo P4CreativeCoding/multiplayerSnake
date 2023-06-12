405b8e7e3551e1f4da2577dedc9d620023cc644f
Leslie Joyce Hartmann <leslie230894@gmail.com>
2023-06-12 22:11:45 +0200

    working snake and canvas drawing

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const BACKGROUND = " #1c0e22";
+const FOOD_COLOUR = "#ffffffff";
+const SNAKE_COLOUR = "#01cfc2";
 
 const gameScreen = document.getElementById("gameScreen");
 
 let canvas, ctx;
+
+// game informations
+let gameState = {
+  player: {
+    pos: {
+      // head position
+      posX: 3,
+      posY: 10,
+    },
+    velocity: {
+      velX: 1,
+      velY: 0,
+    },
+    snakeParts: [
+      { x: 1, y: 10 },
+      { x: 2, y: 10 },
+      { x: 3, y: 10 }, // head
+    ],
+  },
+  food: {
+    posX: 7,
+    posY: 7,
+  },
+  gridSize: 20, // number of pixels in game
+};
 
 function init() {
   // initialisation
@@ -20,4 +47,35 @@ function keydown(keyEvent) {
   console.log(keyEvent.keyCode);
 }
 
+function drawGameCanvas(state) {
+  // paint bg
+  ctx.fillStyle = BACKGROUND;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // variables for positioning
+  const food = state.food;
+  const gridsize = state.gridSize; // game pixel number -> 20
+  const size = canvas.width / gridsize; // game pixel size
+
+  // paint food
+  ctx.fillStyle = FOOD_COLOUR;
+  ctx.fillRect(food.posX * size, food.posY * size, size, size);
+
+  // paint player
+  drawSnake(state.player, size, SNAKE_COLOUR);
+}
+
+function drawSnake(playerState, size, colour) {
+  const snake = playerState.snakeParts;
+
+  ctx.fillStyle = colour;
+
+  // paint every segment of snake
+  for (let cell of snake) {
+    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+  }
+}
+
 init();
+
+drawGameCanvas(gameState);
